20bd5b2d4d393c953b30602b3debb0dff68b16d2
Ujjwal Kumar Agrawal <ujjwalagrawal29102001@gmail.com>
2024-12-26 02:43:23 +0530

    removed standalone and added module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import type { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
